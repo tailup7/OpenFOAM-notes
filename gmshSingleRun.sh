1d2f7919da21868ab89d5ede118724183ad945a4
HP <onouedod@gmail.com>
2025-09-17 19:49:18 +0900

    add

--- gmshSingleRun.sh
+++ gmshSingleRun.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
-set -e  
+set -e  # エラーが出たら中断
 
-gmshToFoam foo.msh
+# === .mshファイル名を取得 ===
+mshfile=$(ls *.msh | head -n 1)
 
-# gmshは単位が無く、mmスケールでメッシュ生成していたため、mスケールに直す
+if [ -z "$mshfile" ]; then
+    echo "cannot find .msh file"
+    exit 1
+fi
+
+echo "=== Using mesh file: $mshfile ==="
+
+# Gmsh形式メッシュをOpenFOAM形式に変換
+gmshToFoam "$mshfile"
+
+# gmshは単位が無く、mmスケールでメッシュ生成していたため、mスケールに直す。(不要ならコメントアウトしてください)
 transformPoints -scale "(1e-3 1e-3 1e-3)"
 
-# constant/polyMesh/boundary ファイル内の "WALL" の "type" を "patch"から "wall" に変更
-sed -i 's/\("WALL"[ \t]*(\|WALL\)/\1/; s/\(type[ \t]*\)patch/\1wall/' constant/polyMesh/boundary
+# checkMesh が OK でなければ 中断。
+checkMesh | tee checkMesh.log
+if ! grep -q "Mesh OK" checkMesh.log; then
+    echo "Mesh check failed. See checkMesh.log"
+    exit 1
+fi
 
-# 計算実行。ログをコンソールに流しながらファイルとしても出力する
+# constant/polyMesh/boundary ファイル内の "WALL" の "type" を "patch"から "wall" に変更
+# これしないとWSSが計算されない。
+sed -i 's/\(WALL.*type[ \t]*\)patch/\1wall/' constant/polyMesh/boundary
+
+# # simpleソルバ実行。ログをコンソールに流しながらファイルとしても出力する
 simpleFoam | tee log
+
+# OpenFOAM は p を p/ρ で計算している (両辺をρで割ったNS式) ので、計算終了後に ρ をかけて正しい圧力値にする
+# (wallShearStressの部分は計算していなければコメントアウトしてください)
+postProcess -func "fieldScale(name=p; scale=1060)"
+postProcess -func "fieldScale(name=wallShearStress; scale=1060)"
